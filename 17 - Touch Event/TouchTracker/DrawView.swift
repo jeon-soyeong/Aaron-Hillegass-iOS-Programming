6f079c38e5997ba2ae22e16a2986f9167363f260
jeon-soyeong <jsu3417@naver.com>
2021-06-02 16:32:23 +0900

    [17] - Touch Event (금메달 과제: 원 그리기)

--- 17 - Touch Event/TouchTracker/DrawView.swift
+++ 17 - Touch Event/TouchTracker/DrawView.swift
@@ -32,14 +32,41 @@ class DrawView: UIView {
     }
     
     func strokeLine(line: Line) {
-        let path = UIBezierPath()
-//        path.lineWidth = 10
-        path.lineWidth = lineThickness
-        path.lineCapStyle = CGLineCap.round
+//        let path = UIBezierPath()
+////        path.lineWidth = 10
+//        path.lineWidth = lineThickness
+//        path.lineCapStyle = CGLineCap.round
+//
+//        path.move(to: line.begin)
+//        path.addLine(to: line.end)
+//        path.stroke()
         
-        path.move(to: line.begin)
-        path.addLine(to: line.end)
-        path.stroke()
+        drawArc(centerPoint: CGPoint(x: min(line.begin.x, line.end.x) + abs((line.end.x - line.begin.x) / 2), y: min(line.begin.y, line.end.y) + abs((line.end.y - line.begin.y) / 2)), startPoint: line.begin, endPoint: line.end, clockwise: true)
+        drawArc(centerPoint: CGPoint(x: min(line.begin.x, line.end.x) + abs((line.end.x - line.begin.x) / 2), y: min(line.begin.y, line.end.y) + abs((line.end.y - line.begin.y) / 2)), startPoint: line.begin, endPoint: line.end, clockwise: false)
+        print("centerPoint: \(CGPoint(x: min(line.begin.x, line.end.x) + abs((line.end.x - line.begin.x) / 2), y: min(line.begin.y, line.end.y) + abs((line.end.y - line.begin.y) / 2)))")
+        
+//        drawArc(centerPoint: CGPoint(x: 100, y: 100), startPoint: CGPoint(x: 50, y: 50), endPoint: CGPoint(x: 250, y: 250), clockwise: true)
+//        drawArc(centerPoint: CGPoint(x: 150, y: 150), startPoint: CGPoint(x: 50, y: 50), endPoint: CGPoint(x: 150, y: 150), clockwise: false)
+    }
+    
+    func drawArc(centerPoint: CGPoint, startPoint: CGPoint, endPoint: CGPoint, clockwise: Bool) {
+        let radius = getRadius(center: centerPoint, start: startPoint)
+        let start = getAngle(center: centerPoint, point: startPoint, radius: radius)
+        let end = getAngle(center: centerPoint, point: endPoint, radius: radius)
+
+        let arcPath = UIBezierPath()
+        arcPath.move(to: startPoint)
+        arcPath.addArc(withCenter: centerPoint, radius: radius, startAngle: start, endAngle: end, clockwise: clockwise)
+
+        let arcLayer = CAShapeLayer()
+        arcLayer.path = arcPath.cgPath
+        arcLayer.lineWidth = 3
+        arcLayer.strokeEnd = 1
+        arcLayer.strokeColor = UIColor.red.cgColor
+        arcLayer.fillColor = UIColor.clear.cgColor
+        arcLayer.fillMode = .forwards
+
+        self.layer.addSublayer(arcLayer)
     }
     
     override func draw(_ rect: CGRect) {
@@ -56,10 +83,10 @@ class DrawView: UIView {
         }
         
 //        UIColor.red.setStroke()
-        currentLineColor.setStroke()
-        for (_, line) in currentLines {
-            strokeLine(line: line)
-        }
+//        currentLineColor.setStroke()
+//        for (_, line) in currentLines {
+//            strokeLine(line: line)
+//        }
         
 //        if let line = currentLine {
 //            UIColor.red.setStroke()
@@ -121,6 +148,26 @@ class DrawView: UIView {
         }
         
         setNeedsDisplay()
+    }
+    
+    func getRadius(center: CGPoint, start: CGPoint) -> CGFloat {
+        let xDist: CGFloat = start.x - center.x
+        let yDist: CGFloat = start.y - center.y
+
+        let radius: CGFloat = sqrt((xDist * xDist) + (yDist * yDist))
+
+        return radius
+    }
+    
+    func getAngle(center: CGPoint, point: CGPoint, radius: CGFloat) -> CGFloat {
+        let origin = CGPoint(x: center.x + radius, y: center.y)
+
+        let v1 = CGVector(dx: origin.x - center.x, dy: origin.y - center.y)
+        let v2 = CGVector(dx: point.x - center.x, dy: point.y - center.y)
+
+        let angle = atan2(v2.dy, v2.dx) - atan2(v1.dy, v1.dx)
+
+        return angle
     }
 }
 
