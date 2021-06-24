67f080b05d0db389f5cda1297bea019732d00e6e
jeon-soyeong <jsu3417@naver.com>
2021-06-25 02:29:17 +0900

    [6] - ProgrammaticViews (금메달 과제: 핀 놓기)

--- 6 - ProgrammaticViews/WorldTrotter/MapViewController.swift
+++ 6 - ProgrammaticViews/WorldTrotter/MapViewController.swift
@@ -12,12 +12,22 @@ import CoreLocation
 class MapViewController: UIViewController {
     var mapView: MKMapView!
     let locationManager = CLLocationManager()
+    var userLocation: MKUserLocation?
+    var changedPin = 0
     
     let zoomButton: UIButton = {
        let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setImage(UIImage(named: "MapIcon"), for: .normal)
         button.addTarget(self, action: #selector(zoom), for: .touchUpInside)
+        return button
+    }()
+    
+    let locationChangeButton: UIButton = {
+       let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "ConvertIcon"), for: .normal)
+        button.addTarget(self, action: #selector(changeLocationPin), for: .touchUpInside)
         return button
     }()
     
@@ -34,8 +44,6 @@ class MapViewController: UIViewController {
         view.addSubview(segmentedControl)
         
         let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor , constant: 8)
-//        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor)
-//        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         
         let margins = view.layoutMarginsGuide
         let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
@@ -47,6 +55,9 @@ class MapViewController: UIViewController {
         
         view.addSubview(zoomButton)
         setUpZoomButtonLayout()
+        
+        view.addSubview(locationChangeButton)
+        setUpLocationChangeButtonLayout()
     }
     
     override func viewDidLoad() {
@@ -56,10 +67,12 @@ class MapViewController: UIViewController {
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
-//        locationManager.startMonitoringSignificantLocationChanges()
         
         mapView.showsUserLocation = true
         mapView.setUserTrackingMode(.follow, animated: true)
+        
+        userLocation = mapView.userLocation
+        addAnnotation()
     }
     
     func setUpZoomButtonLayout() {
@@ -67,6 +80,13 @@ class MapViewController: UIViewController {
         zoomButton.heightAnchor.constraint(equalToConstant: 100).isActive = true
         zoomButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 150).isActive = true
         zoomButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+    }
+    
+    func setUpLocationChangeButtonLayout() {
+        locationChangeButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        locationChangeButton.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        locationChangeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 100).isActive = true
+        locationChangeButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
     }
     
     @objc func mapTypeChanged(segControl: UISegmentedControl) {
@@ -84,12 +104,62 @@ class MapViewController: UIViewController {
     
     @objc func zoom() {
         //현재 위치 가져오기
-        let userLocation = mapView.userLocation
+        guard let location = userLocation else {
+            return
+        }
+        
         //현재 위치를 기준으로 영역을 생성
-        let region = MKCoordinateRegion(center:userLocation.location!.coordinate, latitudinalMeters: 500,
+        let region = MKCoordinateRegion(center: location.location!.coordinate, latitudinalMeters: 500,
                                         longitudinalMeters: 500)
         //맵 뷰의 영역을 설정
         mapView.setRegion(region, animated: true)
+    }
+    
+    @objc func changeLocationPin() {
+        guard let location = userLocation else {
+            return
+        }
+
+        var center = CLLocationCoordinate2D()
+        print("changedPin: \(changedPin)")
+        switch changedPin {
+        case 0:
+            center = CLLocationCoordinate2D(latitude: 37.6658609, longitude: 127.0317674)
+        case 1:
+            center = CLLocationCoordinate2D(latitude: 37.6176125, longitude: 126.9227004)
+        case 2:
+            center = location.location!.coordinate
+        default:
+            break
+        }
+        
+        if changedPin < 2 {
+            changedPin += 1
+        } else {
+            changedPin = 0
+        }
+        
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta:0.01, longitudeDelta:0.01))
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func addAnnotation() {
+        guard let location = userLocation else {
+            return
+        }
+        
+        mapView.addAnnotation(createAnnotation(title: "태어난 곳", coordinate: CLLocationCoordinate2D(latitude: 37.6658609, longitude: 127.0317674)))
+        
+        mapView.addAnnotation(createAnnotation(title: "가본 곳", coordinate: CLLocationCoordinate2D(latitude: 37.6176125, longitude: 126.9227004)))
+        
+        mapView.addAnnotation(createAnnotation(title: "현재", coordinate: CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)))
+    }
+    
+    func createAnnotation(title: String, coordinate: CLLocationCoordinate2D) -> MKPointAnnotation {
+        let point = MKPointAnnotation()
+        point.title = title
+        point.coordinate = coordinate
+        return point
     }
 }
 
@@ -102,7 +172,9 @@ extension MapViewController: CLLocationManagerDelegate {
         // MKCoordinateSpan -- 지도 scale
         let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta:0.01, longitudeDelta:0.01))
 
-        self.mapView.setRegion(region, animated: true)
-        self.locationManager.stopUpdatingLocation()
+        mapView.setRegion(region, animated: true)
+        mapView.addAnnotation(createAnnotation(title: "현재", coordinate: CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)))
+        
+        locationManager.stopUpdatingLocation()
     }
 }
